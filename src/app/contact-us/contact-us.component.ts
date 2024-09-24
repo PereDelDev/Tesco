0f9fc20b268994976f77c3d95cc0d7f7a33ecba2
Pere <pere.pachi9@gmail.com>
2024-09-24 20:05:58 +0200

    formulario del contact us avanzado

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCoffee, faEnvelope, faLocationDot, faLocationPin, faPhone, faPrint } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faLocationDot, faPhone, faPrint } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
@@ -14,4 +15,22 @@ export class ContactUsComponent {
   faPhone = faPhone
   faPrint = faPrint
   faEnvelope = faEnvelope
+
+  Formulario: FormGroup = new FormGroup({
+    //id segun base de datos sin implementar
+    id: new FormControl(null, Validators.required),
+    name: new FormControl(null, Validators.required),
+    company: new FormControl(null, Validators.required),
+    email: new FormControl(null, Validators.required),
+    phone: new FormControl(null, Validators.required),
+    industry: new FormControl(null, Validators.required),
+    service: new FormControl(null, Validators.required),
+    details: new FormControl(null, Validators.required),
+    contact: new FormControl(null, Validators.required)
+  })
+
+  onSubmit() {
+    //Todo: Añadir check de validadores antes de enviar el formulario
+    console.log(this.Formulario.value)
+  }
 }
