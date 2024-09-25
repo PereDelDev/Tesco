c1be74aabbc1d7d9eab3cd4944037e28a8f7dafa
Pere <pere.pachi9@gmail.com>
2024-09-25 11:57:15 +0200

    carrousel terminado

--- src/app/Components/slider/slider.component.ts
+++ src/app/Components/slider/slider.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Company } from '../../interfaces/companies.interface';
+import { COMPANIES } from '../../data/company.data';
 
 @Component({
   selector: 'slider',
@@ -11,58 +13,55 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 export class SliderComponent implements OnInit {
 
-  images = [
-    { src: '../../../assets/img/tesco-bldg.jpg', alt: 'Image 1', link: 'https:example.com/1' },
-    { src: '../../../assets/img/6409a74f-34e7-44f8-9a37-75d8795f1791.jpg', alt: 'Image 2', link: 'https:example.com/2' },
-    { src: '../../../assets/img/dbdff020-60fc-40fe-bb6c-81f7dd86c905.jpg', alt: 'Image 3', link: 'https:example.com/3' },
-    { src: '../../../assets/img/3fe5f42d-ab68-4c8c-8977-b25abf9dd303.jpg', alt: 'Image 4', link: 'https:example.com/4' },
-    { src: '../../../assets/img/tesco-bldg.jpg', alt: 'Image 1', link: 'https:example.com/1' },
-    { src: '../../../assets/img/6409a74f-34e7-44f8-9a37-75d8795f1791.jpg', alt: 'Image 2', link: 'https:example.com/2' },
-    { src: '../../../assets/img/dbdff020-60fc-40fe-bb6c-81f7dd86c905.jpg', alt: 'Image 3', link: 'https:example.com/3' },
-    { src: '../../../assets/img/3fe5f42d-ab68-4c8c-8977-b25abf9dd303.jpg', alt: 'Image 4', link: 'https:example.com/4' },
-    { src: '../../../assets/img/tesco-bldg.jpg', alt: 'Image 1', link: 'https:example.com/1' },
-    { src: '../../../assets/img/6409a74f-34e7-44f8-9a37-75d8795f1791.jpg', alt: 'Image 2', link: 'https:example.com/2' },
-    { src: '../../../assets/img/dbdff020-60fc-40fe-bb6c-81f7dd86c905.jpg', alt: 'Image 3', link: 'https:example.com/3' },
-    { src: '../../../assets/img/3fe5f42d-ab68-4c8c-8977-b25abf9dd303.jpg', alt: 'Image 4', link: 'https:example.com/4' },
-    { src: '../../../assets/img/tesco-bldg.jpg', alt: 'Image 1', link: 'https:example.com/1' },
-    { src: '../../../assets/img/6409a74f-34e7-44f8-9a37-75d8795f1791.jpg', alt: 'Image 2', link: 'https:example.com/2' },
-    { src: '../../../assets/img/dbdff020-60fc-40fe-bb6c-81f7dd86c905.jpg', alt: 'Image 3', link: 'https:example.com/3' },
-    { src: '../../../assets/img/3fe5f42d-ab68-4c8c-8977-b25abf9dd303.jpg', alt: 'Image 4', link: 'https:example.com/4' },
+  arrCompanies: Company[] = COMPANIES
 
 
 
-  ]
   @ViewChild('carouselTrack', { static: false }) carouselTrack!: ElementRef<HTMLDivElement>; // Usamos el nuevo { static: false }
 
-  private isDragging = false;
+  isDragging = false;
   private startPosition = 0;
   private currentTranslate = 0;
   private prevTranslate = 0;
   private animationID: number | undefined;
   private index = 0;
+  isActive = true
 
   ngOnInit() {
     // Iniciamos el loop del carrusel
+    this.index = Math.floor(this.arrCompanies.length / 2); // Empieza en el centro
+
     this.loop();
+  }
+
+  moveLeft() {
+    this.currentTranslate += 200;
+    this.carouselTrack.nativeElement.style.transform = `translateX(${this.currentTranslate}px)`;
+  }
+
+  moveRight() {
+    this.currentTranslate -= 200
+    this.carouselTrack.nativeElement.style.transform = `translateX(${this.currentTranslate}px)`
+  }
+
+  setInitialPosition() {
+    const width = this.carouselTrack.nativeElement.offsetWidth / (this.arrCompanies.length / 10);
+    this.currentTranslate = this.index * -width;
+    this.carouselTrack.nativeElement.style.transform = `translateX(${this.currentTranslate}px)`;
   }
 
   ngAfterViewInit() {
     this.carouselTrack.nativeElement.addEventListener('dragstart', (e) => e.preventDefault());
+    this.setInitialPosition()
   }
 
   onDragStart(event: MouseEvent) {
     this.isDragging = true;
     this.startPosition = event.pageX - this.carouselTrack.nativeElement.offsetLeft;
     this.prevTranslate = this.currentTranslate;
-    // Ensure no text selection while dragging
     document.body.style.userSelect = 'none';
-
-    // Listen globally for mouse movements and mouseup events
     document.addEventListener('mousemove', this.onDrag);
-    document.addEventListener('mouseup', this.onDragEnd);
-
     this.animationID = requestAnimationFrame(this.animate);
-
   }
 
   onDrag(event: MouseEvent) {
@@ -71,42 +70,54 @@ export class SliderComponent implements OnInit {
     const distanceMoved = currentPosition - this.startPosition;
     this.currentTranslate = this.prevTranslate + distanceMoved;
 
+    setTimeout(() => {
+      this.isActive = true
+    }, 2000);
+    console.log(this.onDrag)
   }
 
   onDragEnd() {
     this.isDragging = false;
     cancelAnimationFrame(this.animationID!);
-    // Re-enable text selection
     document.body.style.userSelect = '';
-
-    // Remove global listeners
     document.removeEventListener('mousemove', this.onDrag);
-    document.removeEventListener('mouseup', this.onDragEnd);
-
     this.setPositionByIndex();
-    console.log('funciona')
-
   }
 
 
   animate = () => {
     this.carouselTrack.nativeElement.style.transform = `translateX(${this.currentTranslate}px)`;
     if (this.isDragging) {
-      requestAnimationFrame(this.animate);  // Keep calling animate with correct context
+      requestAnimationFrame(this.animate)
     }
   }
 
   setPositionByIndex() {
-    const width = this.carouselTrack.nativeElement.offsetWidth / this.images.length;
-    this.index = this.currentTranslate < this.prevTranslate ? this.index + 1 : this.index - 1;
-    this.index = Math.max(0, Math.min(this.index, this.images.length - 1)); // Evita que se salga de los límites
+    const width = this.carouselTrack.nativeElement.offsetWidth / this.arrCompanies.length;
+
+    // Calcular el nuevo índice basado en la distancia total movida
+    this.index = Math.round(this.currentTranslate / -width); // Redondear para la imagen más cercana
+
+    // Asegurarse de que el índice no se salga de los límites
+    /*  this.index = Math.max(0, Math.min(this.index, this.arrCompanies.length - 1)); */
+
+    // Calcular la nueva posición de "currentTranslate" basada en el índice ajustado
     this.currentTranslate = this.index * -width;
+
+    // Aplicar la transición de forma suave
+    this.carouselTrack.nativeElement.style.transition = 'transform 0.5s ease';
     this.carouselTrack.nativeElement.style.transform = `translateX(${this.currentTranslate}px)`;
+
+    // Eliminar la transición después de la animación para permitir el arrastre sin retraso
+    setTimeout(() => {
+      this.carouselTrack.nativeElement.style.transition = '';
+    }, 500);
   }
+
 
   loop() {
     setInterval(() => {
-      this.index = (this.index + 1) % this.images.length;
+      this.index = (this.index + 1) % this.arrCompanies.length;
       this.setPositionByIndex();
 
       console.log(this.index)
---
+++ src/app/data/company.data.ts
@@ -0,0 +1,269 @@
+import { Company } from "../interfaces/companies.interface";
+
+export const COMPANIES: Company[] = [
+    {
+        name: 'ACE Tubular Service Limited',
+        foto: '../../assets/img/companies/ACE-Tubular-Service-Limited.jpg'
+    },
+    {
+        name: 'Black bear',
+        foto: '../../assets/img/companies/black-bear-logo1.jpg'
+    },
+    {
+        name: 'Enotec Gas Sensing Solutions',
+        foto: '../../assets/img/companies/Enotec-Gas-Sensing-Solutions.jpg'
+    },
+    {
+        name: 'Eslin',
+        foto: '../../assets/img/companies/eslin-logo-1.jpg'
+    },
+    {
+        name: 'Madgetech',
+        foto: '../../assets/img/companies/madgetech.png'
+    },
+    {
+        name: 'Oilwell',
+        foto: '../../assets/img/companies/oilwell-logo.jpg'
+    },
+    {
+        name: 'PowerChem',
+        foto: '../../assets/img/companies/PowerChem.jpg'
+    },
+    {
+        name: 'TeamModule',
+        foto: '../../assets/img/companies/teamModule-logo.jpg'
+    },
+    {
+        name: 'This',
+        foto: '../../assets/img/companies/this-logo.jpg'
+    },
+    {
+        name: 'Tix Holdings Company',
+        foto: '../../assets/img/companies/TIX-Holdings-Company-Limited.jpg'
+    },
+    {
+        name: 'TNG Energy Services',
+        foto: '../../assets/img/companies/TNG-Energy-Services.jpg'
+    },
+    {
+        name: 'ACE Tubular Service Limited',
+        foto: '../../assets/img/companies/ACE-Tubular-Service-Limited.jpg'
+    },
+    {
+        name: 'Black bear',
+        foto: '../../assets/img/companies/black-bear-logo1.jpg'
+    },
+    {
+        name: 'Enotec Gas Sensing Solutions',
+        foto: '../../assets/img/companies/Enotec-Gas-Sensing-Solutions.jpg'
+    },
+    {
+        name: 'Eslin',
+        foto: '../../assets/img/companies/eslin-logo-1.jpg'
+    },
+    {
+        name: 'Madgetech',
+        foto: '../../assets/img/companies/madgetech.png'
+    },
+    {
+        name: 'Oilwell',
+        foto: '../../assets/img/companies/oilwell-logo.jpg'
+    },
+    {
+        name: 'PowerChem',
+        foto: '../../assets/img/companies/PowerChem.jpg'
+    },
+    {
+        name: 'TeamModule',
+        foto: '../../assets/img/companies/teamModule-logo.jpg'
+    },
+    {
+        name: 'This',
+        foto: '../../assets/img/companies/this-logo.jpg'
+    },
+    {
+        name: 'Tix Holdings Company',
+        foto: '../../assets/img/companies/TIX-Holdings-Company-Limited.jpg'
+    },
+    {
+        name: 'TNG Energy Services',
+        foto: '../../assets/img/companies/TNG-Energy-Services.jpg'
+    },
+    {
+        name: 'ACE Tubular Service Limited',
+        foto: '../../assets/img/companies/ACE-Tubular-Service-Limited.jpg'
+    },
+    {
+        name: 'Black bear',
+        foto: '../../assets/img/companies/black-bear-logo1.jpg'
+    },
+    {
+        name: 'Enotec Gas Sensing Solutions',
+        foto: '../../assets/img/companies/Enotec-Gas-Sensing-Solutions.jpg'
+    },
+    {
+        name: 'Eslin',
+        foto: '../../assets/img/companies/eslin-logo-1.jpg'
+    },
+    {
+        name: 'Madgetech',
+        foto: '../../assets/img/companies/madgetech.png'
+    },
+    {
+        name: 'Oilwell',
+        foto: '../../assets/img/companies/oilwell-logo.jpg'
+    },
+    {
+        name: 'PowerChem',
+        foto: '../../assets/img/companies/PowerChem.jpg'
+    },
+    {
+        name: 'TeamModule',
+        foto: '../../assets/img/companies/teamModule-logo.jpg'
+    },
+    {
+        name: 'This',
+        foto: '../../assets/img/companies/this-logo.jpg'
+    },
+    {
+        name: 'Tix Holdings Company',
+        foto: '../../assets/img/companies/TIX-Holdings-Company-Limited.jpg'
+    },
+    {
+        name: 'TNG Energy Services',
+        foto: '../../assets/img/companies/TNG-Energy-Services.jpg'
+    },
+    {
+        name: 'ACE Tubular Service Limited',
+        foto: '../../assets/img/companies/ACE-Tubular-Service-Limited.jpg'
+    },
+    {
+        name: 'Black bear',
+        foto: '../../assets/img/companies/black-bear-logo1.jpg'
+    },
+    {
+        name: 'Enotec Gas Sensing Solutions',
+        foto: '../../assets/img/companies/Enotec-Gas-Sensing-Solutions.jpg'
+    },
+    {
+        name: 'Eslin',
+        foto: '../../assets/img/companies/eslin-logo-1.jpg'
+    },
+    {
+        name: 'Madgetech',
+        foto: '../../assets/img/companies/madgetech.png'
+    },
+    {
+        name: 'Oilwell',
+        foto: '../../assets/img/companies/oilwell-logo.jpg'
+    },
+    {
+        name: 'PowerChem',
+        foto: '../../assets/img/companies/PowerChem.jpg'
+    },
+    {
+        name: 'TeamModule',
+        foto: '../../assets/img/companies/teamModule-logo.jpg'
+    },
+    {
+        name: 'This',
+        foto: '../../assets/img/companies/this-logo.jpg'
+    },
+    {
+        name: 'Tix Holdings Company',
+        foto: '../../assets/img/companies/TIX-Holdings-Company-Limited.jpg'
+    },
+    {
+        name: 'TNG Energy Services',
+        foto: '../../assets/img/companies/TNG-Energy-Services.jpg'
+    },
+    {
+        name: 'ACE Tubular Service Limited',
+        foto: '../../assets/img/companies/ACE-Tubular-Service-Limited.jpg'
+    },
+    {
+        name: 'Black bear',
+        foto: '../../assets/img/companies/black-bear-logo1.jpg'
+    },
+    {
+        name: 'Enotec Gas Sensing Solutions',
+        foto: '../../assets/img/companies/Enotec-Gas-Sensing-Solutions.jpg'
+    },
+    {
+        name: 'Eslin',
+        foto: '../../assets/img/companies/eslin-logo-1.jpg'
+    },
+    {
+        name: 'Madgetech',
+        foto: '../../assets/img/companies/madgetech.png'
+    },
+    {
+        name: 'Oilwell',
+        foto: '../../assets/img/companies/oilwell-logo.jpg'
+    },
+    {
+        name: 'PowerChem',
+        foto: '../../assets/img/companies/PowerChem.jpg'
+    },
+    {
+        name: 'TeamModule',
+        foto: '../../assets/img/companies/teamModule-logo.jpg'
+    },
+    {
+        name: 'This',
+        foto: '../../assets/img/companies/this-logo.jpg'
+    },
+    {
+        name: 'Tix Holdings Company',
+        foto: '../../assets/img/companies/TIX-Holdings-Company-Limited.jpg'
+    },
+    {
+        name: 'TNG Energy Services',
+        foto: '../../assets/img/companies/TNG-Energy-Services.jpg'
+    },
+    {
+        name: 'ACE Tubular Service Limited',
+        foto: '../../assets/img/companies/ACE-Tubular-Service-Limited.jpg'
+    },
+    {
+        name: 'Black bear',
+        foto: '../../assets/img/companies/black-bear-logo1.jpg'
+    },
+    {
+        name: 'Enotec Gas Sensing Solutions',
+        foto: '../../assets/img/companies/Enotec-Gas-Sensing-Solutions.jpg'
+    },
+    {
+        name: 'Eslin',
+        foto: '../../assets/img/companies/eslin-logo-1.jpg'
+    },
+    {
+        name: 'Madgetech',
+        foto: '../../assets/img/companies/madgetech.png'
+    },
+    {
+        name: 'Oilwell',
+        foto: '../../assets/img/companies/oilwell-logo.jpg'
+    },
+    {
+        name: 'PowerChem',
+        foto: '../../assets/img/companies/PowerChem.jpg'
+    },
+    {
+        name: 'TeamModule',
+        foto: '../../assets/img/companies/teamModule-logo.jpg'
+    },
+    {
+        name: 'This',
+        foto: '../../assets/img/companies/this-logo.jpg'
+    },
+    {
+        name: 'Tix Holdings Company',
+        foto: '../../assets/img/companies/TIX-Holdings-Company-Limited.jpg'
+    },
+    {
+        name: 'TNG Energy Services',
+        foto: '../../assets/img/companies/TNG-Energy-Services.jpg'
+    },
+
+]
